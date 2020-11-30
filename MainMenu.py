9da41934b492ea980221b25290c461d663578b68
Bo Sang <bsang@ualberta.ca>
2020-11-29 23:13:42 -0700

    test delete

--- MainMenu.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-Button(root, text = "Brighten").grid(row=2, column=0)
-Button(root, text = "Darken").grid(row=3, column=0)
-Button(root, text = "Warm").grid(row=4, column=0)
-Button(root, text = "Cool").grid(row=5, column=0)
-root.mainloop()
