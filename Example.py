c8b24aa899a39015dee9dc2cb1d0eaa3963fe7ac
Bo Sang <bsang@ualberta.ca>
2020-11-30 19:47:33 -0700

    data structure changes

---
+++ Example.py
@@ -0,0 +1,22 @@
+"""<html>
+                   <body>
+                   <h1> Financial Report </h1>
+                   <p>Revenue over time: $%d</p>
+                   <p>Revenue breakdown (tips vs. rake): $%d</p>
+                   <p>Revenue per employee over time: $%d</p>
+                   <p>Revenue per player: $%d</p>
+                   <h2> Debts </h2>
+                   <p>Debts outstanding over time: $%d</p>
+                   <p>Debts outstanding as % of float: $%d</p>
+                   <p>Debts outstanding as % of revenue: $%d</p>
+                   <p>Average debt per player: $%d</p>
+                   <p>Average age of debt: $%d</p>
+                   </body>
+                   </html> """
+
+
+table = [["header","text"],["header","text"]]
+
+for i in table:
+    for j in i:
+        if
