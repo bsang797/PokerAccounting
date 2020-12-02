21dab1e792ff5a650bae5a27324ee23f46380c51
Bo Sang <bsang@ualberta.ca>
2020-12-01 18:34:34 -0700

    "added function for creating html file. added reconcliation class"

--- Example.py
+++ Example.py
@@ -1,22 +1,28 @@
-"""<html>
-                   <body>
-                   <h1> Financial Report </h1>
-                   <p>Revenue over time: $%d</p>
-                   <p>Revenue breakdown (tips vs. rake): $%d</p>
-                   <p>Revenue per employee over time: $%d</p>
-                   <p>Revenue per player: $%d</p>
-                   <h2> Debts </h2>
-                   <p>Debts outstanding over time: $%d</p>
-                   <p>Debts outstanding as % of float: $%d</p>
-                   <p>Debts outstanding as % of revenue: $%d</p>
-                   <p>Average debt per player: $%d</p>
-                   <p>Average age of debt: $%d</p>
-                   </body>
-                   </html> """
+import pandas
+import webbrowser
+import os
+from Functions import *
+import Reconciliation as rec
 
+overview_file = os.getcwd()+"/Data/overview.csv"
+people_file = os.getcwd()+"/Data/people.csv"
+roles_file = os.getcwd()+"/Data/roles.csv"
+shift_file = os.getcwd()+"/Data/shift.csv"
+transactions_file = os.getcwd()+"/Data/transactions.csv"
 
-table = [["header","text"],["header","text"]]
+chips_purchased = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).chips_purchased()
+chips_cashed = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).chips_cashed()
+debt_outstanding = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).debt_outstanding_by_player()
+total_rake = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).total_rake()
+total_tips = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).total_tips()
+chips_float = rec.Reconciliation(data_exporter(transactions_file), data_exporter(shift_file), data_exporter(people_file)).chips_floating()
 
-for i in table:
-    for j in i:
-        if
+html_table = [["header","End of Day Reconciliation"],
+              ["chips purchased", str(chips_purchased[0])],
+              ["chips cashed", str(chips_cashed[0])],
+              ["chips out on the tables", str(chips_float[0])],
+              ["debt outstanding", str(debt_outstanding.agg("sum"))],
+              ["total rake", str(total_rake[0])],
+              ["total tips", str(total_tips[0])]]
+
+webbrowser.open(html_temp_file_generator(html_table))
---
+++ Reconciliation.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+class Reconciliation:
+    def __init__(self, transactions, shifts, people):
+        self.transaction = transactions
+        self.shift = shifts
+        self.people = people
+
+    def chips_purchased(self):
+        transaction_is_chip = self.transaction[self.transaction["chip_purchase"] == 1]
+        chip_purchase_bydate = transaction_is_chip.groupby(["datetime"])["quantity"].agg("sum")
+        return chip_purchase_bydate
+
+    def chips_cashed(self):
+        transaction_not_chip = self.transaction[self.transaction["chip_purchase"] == 0]
+        chip_cashed_bydate = transaction_not_chip.groupby(["datetime"])["quantity"].agg("sum")
+        return chip_cashed_bydate
+
+    def chips_floating(self):
+        return self.chips_purchased() + self.chips_cashed() - sum(self.total_rake(), self.total_tips())
+
+    def debt_outstanding_by_player(self):
+        transaction_credit = self.transaction[self.transaction["transaction_type"] == "credit"]
+        debt_outstanding = transaction_credit.groupby(["person_id"])["quantity"].agg("sum")
+        return debt_outstanding
+
+    def total_rake(self):
+        end_float = self.shift.groupby(["datetime"])["end_float"].agg("sum")
+        start_float = self.shift.groupby(["datetime"])["start_float"].agg("sum")
+        return end_float - start_float
+
+    def total_tips(self):
+        tips = self.shift.groupby(["datetime"])["tips"].agg("sum")
+        return tips
